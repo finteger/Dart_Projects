09c8645bec2cf30f00214abad008f3d65d5d077a
finteger <nashassociatesinc@gmail.com>
2025-08-05 14:48:59 -0600

    Update

---
+++ function_arrow_syntax.dart
@@ -0,0 +1,14 @@
+//arrow syntax is shorthand syntax
+
+//traditional block syntax
+add(int a, int b) {
+  return a + b;
+}
+
+//arrow syntax
+var add2 = (int a, int b) => a + b;
+
+//calculates the area of rectangle
+calculateArea(w, h) {
+  return w * h;
+}
